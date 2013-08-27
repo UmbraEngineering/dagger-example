c9f3fed94992ea40392c6abe5e4e5f6e32c92309
James Brumond <james@jbrumond.me>
2013-08-27 01:24:54 -0700

    added model hook support

--- models/users.js
+++ models/users.js
@@ -1,12 +1,37 @@
 
 var app = require('dagger.js').app;
 
-app.models.create('users', {
+var User = app.models.create('users', {
 
 	schema: {
 		name: String,
-		email: app.models.types.Email,
+		email: {type: app.models.types.Email, index: {unique: true}},
 		password: {type: String, select: false}
+	},
+
+	hooks: {
+		// Hash the password if it has been changed
+		'pre::save': function(next) {
+			if (this.isModified('password')) {
+				// 
+				// TODO
+				//   Actually hash passwords
+				// 
+				this.password = 'hashed:' + this.password;
+			}
+
+			next();
+		},
+		// Send an email when new users are created
+		'post::create': function(next) {
+			// 
+			// TODO
+			//   Actually send email
+			// 
+			console.log('Send Email');
+
+			next();
+		}
 	}
 
 });
